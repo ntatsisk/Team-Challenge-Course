fa898f5e9021c23675396123072c6408a6a62693
ntatsisk <k.ntatsis@students.uu.nl>
2019-03-10 22:47:23 +0200

    cropping using spacing

---
+++ calculate_metrics.py
@@ -0,0 +1,46 @@
+import copy
+import numpy as np
+
+def dice_score(ground_truth, prediction):
+    """ Calculate dice score"""
+
+    # Normalize
+    prediction /= np.amax(prediction)
+    ground_truth /= np.amax(ground_truth)
+
+    true_positive_mask = np.logical_and(ground_truth==1, prediction==1)
+    false_positive_mask = np.logical_and(ground_truth==0, prediction==1)
+    false_negative_mask = np.logical_and(ground_truth==1, prediction==0)
+
+    TP = np.count_nonzero(true_positive_mask)
+    FP = np.count_nonzero(false_positive_mask)
+    FN = np.count_nonzero(false_negative_mask)
+
+    DSC = 2*TP / (2*TP + FP + FN)
+
+    return DSC
+
+def optimal_treshold(ground_truth, prediction, low=0.99, high=0.999999, steps=100):
+    """ Find optimal probability threshold that maximizes dice score for a given
+    segmentation prediction"""
+
+    # Range of threshold values to be tested
+    limits = np.linspace(low, high, steps)
+
+    dice_scores = []
+    for limit in limits:
+        thres_seg = copy.deepcopy(prediction)
+        thres_seg[thres_seg>limit] = 1
+        thres_seg[thres_seg<=limit] = 0
+        d = dice_score(ground_truth, thres_seg)
+        dice_scores.append(d)
+
+    # Find the optimal threshold (maximum dice score)
+    opt_pos = np.argmax(dice_scores)
+    opt_threshold = limits[opt_pos]
+    opt_seg = copy.deepcopy(prediction)
+    opt_seg[opt_seg>opt_threshold] = 1
+    opt_seg[opt_seg<=opt_threshold] = 0
+    opt_dice = dice_scores[opt_pos]
+
+    return opt_seg, opt_threshold, opt_dice
